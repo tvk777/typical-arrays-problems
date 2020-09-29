6d8851d35179da342c5d5c139f6222786f216834
tvk777 <klimenko.tv7@gmail.com>
2020-09-29 18:29:05 +0300

    feat: resolve typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length>0 ? array.reduce((min, item) => Math.min(min, item), Infinity) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length>0 ? array.reduce((max, item) => Math.max(max, item), 0) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length===0) return 0;
+  sum = array.reduce((sum, item) => sum + item, 0);
+  return sum/array.length;
 }
